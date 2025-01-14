4af9d0af6d4ec773d23db49b528800aa472dc1c6
Bruno Sant'Anna <mail@bsantanna.me>
2025-01-14 15:40:57 +0100

    adjusts vault container init and tests for integrations endpoint

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,15 @@ from testcontainers.core.waiting_utils import wait_for_logs
 from testcontainers.redis import RedisContainer
 from testcontainers.vault import VaultContainer
 
-redis = RedisContainer("redis:alpine").with_exposed_ports(16379)
-vault = VaultContainer("hashicorp/vault:1.18.1").with_exposed_ports(18200)
+redis = RedisContainer("redis:alpine").with_bind_ports(container=6379, host=16379)
+
+vault = (
+    VaultContainer("hashicorp/vault:1.18.1")
+    .with_bind_ports(container=8200, host=18200)
+    .with_env("VAULT_DEV_ROOT_TOKEN_ID", "dev-only-token")
+    .with_env("VAULT_DEV_LISTEN_ADDRESS", "0.0.0.0:8200")
+)
+
 
 os.environ["TESTING"] = "1"
 
@@ -22,5 +29,7 @@ def test_config(request):
 
     request.addfinalizer(remove_container)
     wait_for_logs(redis, "Ready to accept connections")
-
+    wait_for_logs(
+        vault, "Development mode should NOT be used in production installations!"
+    )
     yield
--- tests/integration/test_integrations_endpoint.py
+++ tests/integration/test_integrations_endpoint.py
@@ -9,68 +9,73 @@ def client():
     yield TestClient(app)
 
 
-# class TestAgentsEndpoints:
-#     @pytest.mark.asyncio
-#     async def test_get_list(self, client):
-#         # when
-#         response = client.get("/agents/list")
-#
-#         # then
-#         assert response.status_code == 200
-#         assert isinstance(response.json(), list)
-#
-#     @pytest.mark.asyncio
-#     async def test_get_by_id_success(self, client):
-#         # given
-#         response = client.post("/agents/create")
-#         agent_id = response.json()["id"]
-#
-#         # when
-#         response_2 = client.get(f"/agents/{agent_id}")
-#
-#         # then
-#         assert response_2.status_code == 200
-#         assert "id" in response_2.json()
-#
-#     @pytest.mark.asyncio
-#     async def test_get_by_id_not_found(self, client):
-#         # given
-#         agent_id = 9999
-#
-#         # when
-#         response = client.get(f"/agents/{agent_id}")
-#
-#         # then
-#         assert response.status_code == 404
-#
-#     @pytest.mark.asyncio
-#     async def test_add_item(self, client):
-#         # when
-#         response = client.post("/agents/create")
-#
-#         # then
-#         assert response.status_code == 201
-#         assert "id" in response.json()
-#
-#     @pytest.mark.asyncio
-#     async def test_remove_agent_success(self, client):
-#         # given
-#         response = client.post("/agents/create")
-#         agent_id = response.json()["id"]
-#
-#         # when
-#         response_2 = client.delete(f"/agents/delete/{agent_id}")
-#
-#         # then
-#         assert response_2.status_code == 204
-#
-#     @pytest.mark.asyncio
-#     async def test_remove_agent_not_found(self, client):
-#         # given
-#         agent_id = 9999
-#
-#         # when
-#         response = client.delete(f"/agents/delete/{agent_id}")
-#
-#         # then
-#         assert response.status_code == 404
+class TestIntegrationsEndpoints:
+    @pytest.mark.asyncio
+    async def test_get_list(self, client):
+        # when
+        response = client.get("/integrations/list")
+
+        # then
+        assert response.status_code == 200
+        assert isinstance(response.json(), list)
+
+    @pytest.mark.asyncio
+    async def test_create_and_get_by_id_success(self, client):
+        # given
+        response = client.post(
+            url="/integrations/create",
+            json={
+                "api_endpoint": "https://example.com",
+                "api_key": "an_invalid_key",
+                "integration_type": "openai_api_v1",
+            },
+        )
+        integration_id = response.json()["id"]
+
+        # when
+        response_2 = client.get(f"/integrations/{integration_id}")
+
+        # then
+        assert response_2.status_code == 200
+        assert "id" in response_2.json()
+
+    @pytest.mark.asyncio
+    async def test_get_by_id_not_found(self, client):
+        # given
+        integration_id = "not_existing_id"
+
+        # when
+        response = client.get(f"/integrations/{integration_id}")
+
+        # then
+        assert response.status_code == 404
+
+    @pytest.mark.asyncio
+    async def test_create_and_delete_success(self, client):
+        # given
+        response = client.post(
+            url="/integrations/create",
+            json={
+                "api_endpoint": "https://example.com",
+                "api_key": "an_invalid_key",
+                "integration_type": "openai_api_v1",
+            },
+        )
+        integration_id = response.json()["id"]
+
+        # when
+        response_2 = client.delete(f"/integrations/delete/{integration_id}")
+
+        # then
+        assert response_2.status_code == 204
+
+    @pytest.mark.asyncio
+    async def test_remove_not_found(self, client):
+        # given
+        integration_id = "not_existing_id"
+
+        # when
+        response = client.delete(f"/integrations/delete/{integration_id}")
+
+        # then
+        assert response.status_code == 404
