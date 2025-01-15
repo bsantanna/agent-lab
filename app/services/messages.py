0b5ed1ef5977c31228ad5f092d6799e88ca37523
Bruno Sant'Anna <mail@bsantanna.me>
2025-01-15 23:49:45 +0100

    adjusts message service

--- app/services/messages.py
+++ app/services/messages.py
@@ -1,5 +1,4 @@
 from typing import Iterator
-from uuid import uuid4
 
 from app.domain.models import Message
 from app.domain.repositories.messages import MessageRepository
@@ -9,15 +8,25 @@ class MessageService:
     def __init__(self, message_repository: MessageRepository) -> None:
         self._repository: MessageRepository = message_repository
 
-    def get_messages(self) -> Iterator[Message]:
-        return self._repository.get_all()
+    def get_messages(self, agent_id: str) -> Iterator[Message]:
+        return self._repository.get_all(agent_id)
 
-    def get_message_by_id(self, message_id: int) -> Message:
+    def get_message_by_id(self, message_id: str) -> Message:
         return self._repository.get_by_id(message_id)
 
-    def create_message(self) -> Message:
-        uid = uuid4()
-        return self._repository.add(email=f"{uid}@email.com", password="pwd")
+    def create_message(
+        self,
+        message_role: str,
+        message_content: str,
+        agent_id: str,
+        attachment_id: str = None,
+    ) -> Message:
+        return self._repository.add(
+            message_role=message_role,
+            message_content=message_content,
+            agent_id=agent_id,
+            attachment_id=attachment_id,
+        )
 
-    def delete_message_by_id(self, message_id: int) -> None:
+    def delete_message_by_id(self, message_id: str) -> None:
         return self._repository.delete_by_id(message_id)
