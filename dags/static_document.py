2a086a5b080d16eccb53c28f530a3432de2279a5
Bruno Sant'Anna <mail@bsantanna.me>
2025-02-17 10:31:11 +0100

    file type processing tasks

--- dags/static_document.py
+++ dags/static_document.py
@@ -1,51 +1,99 @@
 from airflow import DAG
 from airflow.decorators import task
-from kubernetes.client import V1Volume, V1VolumeMount, V1PersistentVolumeClaimVolumeSource
+from kubernetes.client import (
+    V1Volume,
+    V1VolumeMount,
+    V1PersistentVolumeClaimVolumeSource,
+)
 from datetime import datetime
 
 default_args = {
-    'owner': 'airflow',
-    'depends_on_past': False,
-    'start_date': datetime(2025, 1, 1),
-    'retries': 1,
+    "owner": "airflow",
+    "depends_on_past": False,
+    "start_date": datetime(2025, 1, 1),
+    "retries": 1,
 }
 
 dag = DAG(
-    'static_document_data',
+    "static_document_data",
     default_args=default_args,
-    schedule_interval='@daily',
+    schedule_interval="@daily",
     catchup=False,
 )
 
 volume = V1Volume(
-    name='network-data',
+    name="network-data",
     persistent_volume_claim=V1PersistentVolumeClaimVolumeSource(
-        claim_name='nfs-data-claim'
-    )
+        claim_name="nfs-data-claim"
+    ),
 )
 
 volume_mount = V1VolumeMount(
-    name='network-data',
-    mount_path='/mnt/data',
-    sub_path=None,
-    read_only=False
+    name="network-data", mount_path="/mnt/data", sub_path=None, read_only=False
 )
+
 
 @task.kubernetes(
     image="bsantanna/compute-document-utils",
     namespace="compute",
     volumes=[volume],
-    volume_mounts=[volume_mount]
+    volume_mounts=[volume_mount],
 )
 def process_files():
     import os
-    files = {'pptx': [], 'docx': [], 'pdf': [], 'jpg': [], 'json': []}
-    for root, _, filenames in os.walk('/mnt/data'):
+
+    files = {"pptx": [], "docx": [], "pdf": [], "jpg": []}
+    for root, _, filenames in os.walk("/mnt/data"):
         for filename in filenames:
             ext = os.path.splitext(filename)[1].lower()[1:]
             if ext in files:
                 files[ext].append(os.path.join(root, filename))
-    print(files)
+
+    process_pptx_files(files["pptx"])
+    process_docx_files(files["docx"])
+    process_pdf_files(files["pdf"])
+    process_jpg_files(files["jpg"])
+
+
+@task.kubernetes(
+    image="bsantanna/compute-document-utils",
+    namespace="compute",
+    volumes=[volume],
+    volume_mounts=[volume_mount],
+)
+def process_pptx_files(pptx_files):
+    print(f"Processing the following pptx files: {pptx_files}")
+
+
+@task.kubernetes(
+    image="bsantanna/compute-document-utils",
+    namespace="compute",
+    volumes=[volume],
+    volume_mounts=[volume_mount],
+)
+def process_docx_files(docx_files):
+    print(f"Processing the following docx files: {docx_files}")
+
+
+@task.kubernetes(
+    image="bsantanna/compute-document-utils",
+    namespace="compute",
+    volumes=[volume],
+    volume_mounts=[volume_mount],
+)
+def process_pdf_files(pdf_files):
+    print(f"Processing the following pdf files: {pdf_files}")
+
+
+@task.kubernetes(
+    image="bsantanna/compute-document-utils",
+    namespace="compute",
+    volumes=[volume],
+    volume_mounts=[volume_mount],
+)
+def process_jpg_files(jpg_files):
+    print(f"Processing the following jpg files: {jpg_files}")
+
 
 with dag:
     process_files()
