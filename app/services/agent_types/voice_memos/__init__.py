f7727b717ed0c7f719bd5e5906b2d99041515bc4
Bruno Sant'Anna <mail@bsantanna.me>
2025-04-10 07:27:38 +0200

    loads audio file as base64 into prompt

--- app/services/agent_types/voice_memos/__init__.py
+++ app/services/agent_types/voice_memos/__init__.py
@@ -1,4 +1,14 @@
 SUPERVISED_AGENT_CONFIGURATION ={
+    "content_analyst": {
+        "name": "content_analyst",
+        "desc": (
+            "Responsible for mapping relevant information, understanding user needs and conducting content analysis"
+        ),
+        "desc_for_llm": (
+            "Outputs a Markdown report with findings."
+        ),
+        "is_optional": False,
+    },
     "reporter": {
         "name": "reporter",
         "desc": (
