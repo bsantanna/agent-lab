6f591ff58242576fcd0497c15065d21811242657
Bruno Sant'Anna <mail@bsantanna.me>
2025-04-25 20:37:57 +0200

    introduces chat component

--- frontend/console/src/app/pages/dialog/dialog.component.ts
+++ frontend/console/src/app/pages/dialog/dialog.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import {HeaderComponent} from '../../components/header/header.component';
 import {QueryInputComponent} from '../../components/query-input/query-input.component';
+import {ChatComponent} from '../../components/chat/chat.component';
 
 @Component({
   selector: 'console-dialog',
   imports: [
+    ChatComponent,
     HeaderComponent,
     QueryInputComponent
   ],
