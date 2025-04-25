415daf0e8245f9831094ea087d50ca9c2d49025b
Bruno Sant'Anna <mail@bsantanna.me>
2025-04-25 21:01:45 +0200

    prototypes chat  (2)

--- frontend/console/src/app/pages/dialog/dialog.component.ts
+++ frontend/console/src/app/pages/dialog/dialog.component.ts
@@ -1,7 +1,12 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component} from '@angular/core';
 import {HeaderComponent} from '../../components/header/header.component';
 import {QueryInputComponent} from '../../components/query-input/query-input.component';
 import {ChatComponent} from '../../components/chat/chat.component';
+import {Store} from '@ngrx/store';
+import {selectAll as selectAllMessages, selectMessageIsLoading} from '../../store/message/message.selectors';
+import {ActivatedRoute} from '@angular/router';
+import {MessageActions} from '../../store/message/message.actions';
+import {MessageListRequest} from '../../openapi';
 
 @Component({
   selector: 'console-dialog',
@@ -13,6 +18,24 @@ import {ChatComponent} from '../../components/chat/chat.component';
   templateUrl: './dialog.component.html',
   styleUrl: './dialog.component.scss'
 })
-export class DialogComponent {
+export class DialogComponent implements AfterViewInit {
+
+  readonly isProcessing$;
+  readonly messages$;
+
+  constructor(
+    private readonly store: Store,
+    private readonly activatedRoute: ActivatedRoute,
+  ) {
+    this.isProcessing$ = this.store.select(selectMessageIsLoading);
+    this.messages$ = this.store.select(selectAllMessages);
+  }
+
+  ngAfterViewInit(): void {
+    const messageListRequest = {
+      agent_id: this.activatedRoute.snapshot.params['agentId']
+    } as MessageListRequest;
+    this.store.dispatch(MessageActions.loadMessages({data: messageListRequest}));
+  }
 
 }
