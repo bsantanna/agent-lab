47b90ef772db5084a42b74f56526ecca19617bad
Bruno Sant'Anna <mail@bsantanna.me>
2025-08-02 20:39:59 +0200

    feat: adds non numeric chars for schema / id

--- app/infrastructure/auth/user.py
+++ app/infrastructure/auth/user.py
@@ -5,7 +5,7 @@ async def map_user(userinfo: dict) -> User | None:
     user_id = userinfo.get("sub")
     if user_id is not None:
         return User(
-            id=user_id,
+            id=f"id_{user_id}",
             email=userinfo.get("email"),
             username=userinfo.get("preferred_username"),
         )
