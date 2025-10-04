2c286e7f1f655b55e4e510a5fe43944f141ddc3c
Bruno Sant'Anna <837854+bsantanna@users.noreply.github.com>
2025-10-04 22:29:31 +0200

    Fix: project maintenance cycle (#233)
    * fix: removes frontend code
    
    * fix: documentation MCP notebook reference
    
    * fix: researcher guide documentation

--- frontend/console/src/app/store/attachment/attachment.selectors.ts
+++
@@ -1,9 +0,0 @@
-import {createFeatureSelector, createSelector} from '@ngrx/store';
-import {AttachmentState} from './attachment.reducer';
-
-export const selectAttachmentState = createFeatureSelector<AttachmentState>('attachment');
-
-export const selectCurrentAttachment = createSelector(
-  selectAttachmentState,
-  state => state.attachment
-)
