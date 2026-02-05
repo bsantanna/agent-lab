b068fb35454d76ffea4fb0d97553d5a4ca194a31
Bruno Sant'Anna <mail@bsantanna.me>
2026-02-05 18:43:53 +0100

    fix: tests adjusts 5

--- tests/simulation/conftest.py
+++ tests/simulation/conftest.py
@@ -1,12 +1,14 @@
 import time
 
+import pytest
+
 _last_test_time = 0
 
-
-def pytest_runtest_setup(item):
-    global _last_test_time
-    if _last_test_time > 0:
-        elapsed = time.time() - _last_test_time
-        if elapsed < 120:
-            time.sleep(120 - elapsed)
-    _last_test_time = time.time()
+@pytest.fixture(autouse=True)
+def throttle_simulation_tests():
+  global _last_test_time
+  if _last_test_time > 0:
+      elapsed = time.time() - _last_test_time
+      if elapsed < 60:
+          time.sleep(60 - elapsed)
+  _last_test_time = time.time()
